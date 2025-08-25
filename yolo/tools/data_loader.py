08f65c7dbbae100518b3d7c1f430aec9d41b9905
matthewasloan95 <matthewasloan95@gmail.com>
2025-08-24 23:01:13 -0400

    fix dataloader shuffle regression

--- yolo/tools/data_loader.py
+++ yolo/tools/data_loader.py
@@ -242,6 +242,7 @@ def create_dataloader(data_cfg: DataConfig, dataset_cfg: DatasetConfig, task: st
         num_workers=data_cfg.cpu_num,
         pin_memory=data_cfg.pin_memory,
         collate_fn=collate_fn,
+        shuffle=data_cfg.shuffle,
     )
 
 
